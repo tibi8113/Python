818a34b4f59b8ed2033236522893f60fd3792267
H012982\ik012982i9 <ii.tibi@hotmail.com>
2017-03-30 08:32:57 +0200

    changes

--- menu.py
+++ menu.py
@@ -2,36 +2,6 @@ import misfunciones as m
 import json
 
 
-def buscar_zona():
-    for z, p in m.zonas.items():
-        print(z)
-        for d in p:
-            if d == destino:
-                zona = z
-                break
-    return zona
-
-
-def print_zonas():
-    for z, p in m.zonas.items():
-        print(p)
-
-
-def print_precios():
-    for p in m.precios.values():
-        print(p)
-
-def get_precio(zonas):
-    if zonas >= 1 and zonas <= 6:
-        return m.precios[zonas - 1]
-    else:
-        return -1
-
-mizona = 1
-mipoblacion = "Irun"
-
-
-
 """MAIN"""
 
 opc = input("1.Ida\n2.Ida y vuelta\n3.Mensual\n")
--- misfunciones.py
+++ misfunciones.py
@@ -3,7 +3,7 @@ precios = {
     "ida y vuelta" : [2.60, 2.75, 4.10, 5.50, 6.40, 8.00],
     "mensual" : [34.45, 43.95, 58.65, 73.30, 85.80, 91.00],
     "mensual ilimitado" : [45.60, 51.70, 73.00, 95.85, 109.55, 144.60],
-}
+          }
 
 zonas = {
     "1" : ["Urnieta","Hernani Centro","Hernani","Martutene","Loiola","Donosti","Gros","Ategorrieta","Intxaurrondo","Herrera","Pasaia","Lezo-Renteria"],
@@ -13,4 +13,31 @@ zonas = {
     "5" : ["Legazpi","Zumarraga","Ormaiztegi"],
     "6" : ["Brinkola"],
         }
-        
+
+def buscar_zona():
+    for z, p in zonas.items():
+        print(z)
+        for d in p:
+            if d == destino:
+                zona = z
+                break
+    return zona
+
+
+def print_zonas():
+    for z, p in zonas.items():
+        print(p)
+
+
+def print_precios():
+    for p in precios.values():
+        print(p)
+
+def get_precio(zonas):
+    if zonas >= 1 and zonas <= 6:
+        return precios[zonas - 1]
+    else:
+        return -1
+
+mizona = 1
+mipoblacion = "Irun"
