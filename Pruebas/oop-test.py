cfc46637d59ad7f74a25a0185f386755df5216f1
H012982\ik012982i9 <ii.tibi@hotmail.com>
2017-05-05 09:02:36 +0200

    Changes

--- Pruebas/oop-test.py
+++ Pruebas/oop-test.py
@@ -11,6 +11,7 @@ class Car():
     def __str__(self):
         return "Car - Make: %s, Model: %s, Color: %s, Fuel: %s" % (self.make, self.model, self.color, self.fuel_level)
 
+    #Repostar
     def add_fuel(self, amount):
         """Add fuel to the tank."""
         if (self.fuel_level + amount <= self.fuel_capacity):
@@ -19,16 +20,54 @@ class Car():
         else:
             print("The tank won't hold that much.")
 
-""" Crear los objetos """
+class ElectricCar(Car):
+    def __init__(self, make, model, year, color):
+        super().__init__(make, model, year, color)
+        #Tamaño de la bateria.
+        self.battery_size = 100
+        #Nivel de bateria en %.
+        self.charge_level = 0
+
+class WareHouse():
+    def __init__(self, name, location)
+        self.name = name
+        self.location = location
+        self.cars = []
+
+    #Añadir un coche.
+    def add_car(self, car):
+        self.cars.append(car)
+
+    #Quitar un coche.
+    def remove_car(self, car):
+        self.cars.append(car)
+
+
+"""-----------------------------------------------------------------------------------------------------------------------------------"""
+
+""" Crear coches """
 my_car = Car('Audi' , 'A4' , 2015 , 'Red')
 my_car2 = Car('Ford' , 'Mustang' , 2016)
+my_electric_car = ElectricCar('Tesla' , 'S' , 2016, 'Cian')
 
+"""-----------------------------------------------------------------------------------------------------------------------------------"""
+
+""" Crear metodos """
 my_car.add_fuel(50)
+
+"""-----------------------------------------------------------------------------------------------------------------------------------"""
 
 print(my_car)
 print(my_car2)
 
+#Lista de coches
+lista_coches = []
+lista_coches.append(my_car)
+lista_coches.append(my_car2)
 
+#Print de todos los coches
+for coche in lista_coches:
+    print(coche)
 
 #print("Make: " + my_car.make + "; " + "Model: " + my_car.model + "; " + "Color: " + my_car.color)
 #print("Make: " + my_car2.make + "; " + "Model: " + my_car2.model + "; " + "Color: " + my_car2.color)
